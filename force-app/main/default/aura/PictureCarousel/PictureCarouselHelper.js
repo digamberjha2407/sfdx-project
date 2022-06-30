3eee5c22c7cbf6ebb9a35b5d0aa7d5ebb80c1eb9
DIGAMBER JHA <90133053+digamberjha2407@users.noreply.github.com>
2022-06-30 11:53:36 +0530

    11:53 30th june

--- force-app/main/default/aura/PictureCarousel/PictureCarouselHelper.js
+++ force-app/main/default/aura/PictureCarousel/PictureCarouselHelper.js
@@ -2,5 +2,6 @@
     setSlideWidth: function (component) {
         var slideWidth = component.find("gallery").getElement().offsetWidth;
         component.set("v.slideWidth", slideWidth);
+        //comment at 11:53 30th june
     }
 })
